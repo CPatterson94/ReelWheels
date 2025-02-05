c56d80d19c9bc7f4588a6cd28f04e1708e3e6a9d
Curtis Patterson <Dpatter94@yahoo.com>
2025-02-05 16:14:56 -0500

    Update Footer component.
    Upgrade Prisma dependencies to version 6.3.1

--- client/src/components/directory/Footer.jsx
+++ client/src/components/directory/Footer.jsx
@@ -1,32 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Footer with link to Github and About Us page
-function Footer(){
-    return(
-        <footer>
-            <a
-          href="https://github.com/ojgraham1/ReelWheels"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="git"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            size="2x"
-            style={{ color: "#ffff" }}
-          />
-        </a>
-        <NavLink
-            to="/aboutus"
-            className="aboutus"
-        >
-            About Us
-        </NavLink>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/CPatterson94/ReelWheels"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="git"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2x" style={{ color: "#ffff" }} />
+      </a>
+      <NavLink to="/aboutus" className="aboutus">
+        About Us
+      </NavLink>
+    </footer>
+  );
 }
 
 export default Footer;
